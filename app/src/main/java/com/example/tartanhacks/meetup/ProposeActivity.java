1a8e2fac3f39a9eb870415b3f7e9a0bef78726fa
edgykuma <mailbox_eded159@yahoo.com>
2016-02-06 03:45:33 -0500

    Added activity name input and category drop-down menu to ProposeActivity.

--- app/src/main/java/com/example/tartanhacks/meetup/ProposeActivity.java
+++ app/src/main/java/com/example/tartanhacks/meetup/ProposeActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Adapter;
 
 public class ProposeActivity extends AppCompatActivity {
 
@@ -18,16 +21,16 @@ public class ProposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_propose);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
 }
