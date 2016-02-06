98b80fe0f0c9d2b3ebfacc43554edc3d7cb4d771
edgykuma <mailbox_eded159@yahoo.com>
2016-02-06 11:47:49 -0500

    Finished checkbox functionality for activity selection. Still not linked to any data yet.

--- app/src/main/java/com/example/tartanhacks/meetup/SearchActivity.java
+++ app/src/main/java/com/example/tartanhacks/meetup/SearchActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CheckBox;
 
 public class SearchActivity extends AppCompatActivity {
     Intent intent = getIntent();
@@ -19,5 +20,69 @@ public class SearchActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
 
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkbox_outdoor:
+                /*
+                if (checked)
+                // Do what when checked?
+                else
+                // If not checked?
+                */
+                break;
+            case R.id.checkbox_movies:
+                /*
+                if (checked)
+                // Do what when checked?
+                else
+                // If not checked?
+                */
+                break;
+            case R.id.checkbox_shopping:
+                /*
+                if (checked)
+                // Do what when checked?
+                else
+                */
+                // If not checked?
+                break;
+            case R.id.checkbox_sports:
+                /*
+                if (checked)
+                // Do what when checked?
+                else
+                // If not checked?
+                */
+                break;
+            case R.id.checkbox_music:
+                /*
+                if (checked)
+
+                // Do what when checked?
+                else
+                // If not checked?
+                */
+                break;
+            case R.id.checkbox_dining:
+                /*
+                if (checked)
+                // Do what when checked?
+                else
+                // If not checked?
+                */
+                break;
+            case R.id.checkbox_clubbing:
+                /*
+                if (checked)
+                // Do what when checked?
+                else
+                // If not checked?
+                */
+                break;
+        }
+    }
 }
