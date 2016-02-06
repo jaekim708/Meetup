ebd026417b4e610af5ae6ce8c39cca501714c4ea
edgykuma <mailbox_eded159@yahoo.com>
2016-02-06 05:47:35 -0500

    Finished ProposeActivity, all user inputs will now parse to server.

--- app/src/main/java/com/example/tartanhacks/meetup/ProposeActivity.java
+++ app/src/main/java/com/example/tartanhacks/meetup/ProposeActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Adapter;
+
+import com.parse.ParseObject;
 
 public class ProposeActivity extends AppCompatActivity {
 
@@ -31,6 +34,35 @@ public class ProposeActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        String activityType = spinner.getSelectedItem().toString();
     }
 
+    public void sendParseData(View v)   {
+        // get EditText by id
+        EditText grabName = (EditText) findViewById(R.id.activity_name);
+        // Store EditText in Variable
+        String activityName = grabName.getText().toString();
+
+        EditText grabNum = (EditText) findViewById(R.id.number_people);
+        int numPeople = Integer.parseInt(grabNum.getText().toString());
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        String activityType = spinner.getSelectedItem().toString();
+
+        UserActivity userActivity = new UserActivity();
+        userActivity.put("name", activityName);
+        userActivity.put("numOfPeople", numPeople);
+        userActivity.put("category", activityType);
+        userActivity.saveInBackground();
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
 }
