78e3941b2cc857642db21f624fffb48538cb8be9
jaekim708 <jaekim708@gmail.com>
2016-02-06 02:04:41 -0500

    Can pull info from parse, see swipeactivity

--- app/src/main/java/com/example/tartanhacks/meetup/MainActivity.java
+++ app/src/main/java/com/example/tartanhacks/meetup/MainActivity.java
@@ -3,34 +3,14 @@ package com.example.tartanhacks.meetup;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
 import android.content.Intent;
 import android.view.View;
-import android.widget.EditText;
-import com.parse.ParseQuery;
-
-import java.util.List;
-
 
 public class MainActivity extends Activity {
-    ParseQuery<ParseObject> activities_query = ParseQuery.getQuery("UserActivity");
-    ParseObject activities;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        activities_query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> objects, ParseException e) {
-                if (e == null) {
-                    activities = objects.get(0); // should only be one UserActivity object
-                } else {
-                    // shouldn't be here...
-                }
-            }
-        });
     }
 
     /* This will be called when the user clicks the "Propose" button. */
