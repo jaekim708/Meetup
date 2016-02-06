eebcbaf6b6fe485c0be0bfc83fb3a9867e02c946
jaekim708 <jaekim708@gmail.com>
2016-02-06 03:28:41 -0500

    Putting in tinder style swipes, need testing

--- app/src/main/java/com/example/tartanhacks/meetup/UserActivity.java
+++ app/src/main/java/com/example/tartanhacks/meetup/UserActivity.java
@@ -3,6 +3,8 @@ package com.example.tartanhacks.meetup;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
 
 @ParseClassName("UserActivity")
 public class UserActivity extends ParseObject {
@@ -12,6 +14,22 @@ public class UserActivity extends ParseObject {
 
     public void setName(String value) {
         put("name", value);
+    }
+
+    public ParseRelation<ParseUser> getUsersLiked(){
+        return getRelation("usersLiked");
+    }
+
+    public void addUsersLiked(ParseUser user){
+        getUsersLiked().add(user);
+    }
+
+    public ParseRelation<ParseUser> getUsersDisliked(){
+        return getRelation("usersLiked");
+    }
+
+    public void addUsersDisliked(ParseUser user){
+        getUsersDisliked().add(user);
     }
 
     public String getCategory() {
